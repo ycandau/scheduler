f2a233a8059ddfbf2665c5897e39108b386e3d4f
Yves Candau <ycandau@sfu.ca>
2021-08-02 15:01:28 -0700

    Add button React component

--- src/components/Button.js
+++ src/components/Button.js
@@ -2,6 +2,21 @@ import React from 'react';
 
 import 'components/Button.scss';
 
-export default function Button(props) {
-  return <></>;
-}
+const Button = function (props) {
+  const buttonClass =
+    'button' +
+    (props.confirm ? ' button--confirm' : '') +
+    (props.danger ? ' button--danger' : '');
+
+  return (
+    <button
+      className={buttonClass}
+      onClick={props.onClick}
+      disabled={props.disabled}
+    >
+      {props.children}
+    </button>
+  );
+};
+
+export default Button;
