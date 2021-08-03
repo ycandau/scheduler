1681c3abe46fcc986547f484f6b1b732da091316
Yves Candau <ycandau@sfu.ca>
2021-08-03 02:10:13 -0700

    Add state to the Appointment > Form component

--- src/components/Appointment/Form.js
+++ src/components/Appointment/Form.js
@@ -4,7 +4,12 @@ import Button from '../Button';
 import InterviewerList from '../InterviewerList';
 
 //------------------------------------------------------------------------------
-// Props:
+// Props | Created:
+//   - interviewers: Array
+//   - onSave(): Function
+//   - onCancel(): Function
+//
+// Props | Edit:
 //   - name: String
 //   - interviewers: Array
 //   - interviewer: Number
@@ -20,33 +25,48 @@ import InterviewerList from '../InterviewerList';
 //   - setInterviewer(): Function
 
 const Form = function (props) {
+  const [name, setName] = useState(props.name || '');
+  const [interviewer, setInterviewer] = useState(props.interviewer || null);
+
+  const reset = () => {
+    setName('');
+    setInterviewer(null);
+  };
+
+  const cancel = () => {
+    reset();
+    props.onCancel();
+  };
+
+  const save = () => {
+    props.onSave(name, interviewer);
+  };
+
   return (
     <main className="appointment__card appointment__card--create">
       <section className="appointment__card-left">
-        <form autoComplete="off">
+        <form autoComplete="off" onSubmit={(event) => event.preventDefault()}>
           <input
             className="appointment__create-input text--semi-bold"
             name="name"
             type="text"
             placeholder="Enter Student Name"
-            value={props.name}
-            /*
-          This must be a controlled component
-        */
+            value={name}
+            onChange={(event) => setName(event.target.value)}
           />
         </form>
         <InterviewerList
           interviewers={props.interviewers}
-          value={props.interviewer}
-          onChange={null}
+          value={interviewer}
+          onChange={setInterviewer}
         />
       </section>
       <section className="appointment__card-right">
         <section className="appointment__actions">
-          <Button onClick={props.onCancel} danger>
+          <Button onClick={cancel} danger>
             Cancel
           </Button>
-          <Button onClick={props.onSave} confirm>
+          <Button onClick={save} confirm>
             Save
           </Button>
         </section>
