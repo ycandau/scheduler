3c2f8fd636b3c6c8c753dbe3ffd663e631b3e012
Yves Candau <ycandau@sfu.ca>
2021-08-03 00:06:02 -0700

    Add Appointment component

---
+++ src/components/Appointment/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import './styles.scss';
+
+//------------------------------------------------------------------------------
+// Props:
+//   -
+
+const Appointment = function (props) {
+  return <article className="appointment"></article>;
+};
+
+export default Appointment;
