59aaacef9bd437f2419a48b4fbc2ffd9c51edc3b
Yves Candau <ycandau@sfu.ca>
2021-08-03 13:57:25 -0700

    Add conditional Appointment display between Show and Empty

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -2,12 +2,35 @@ import React from 'react';
 
 import './styles.scss';
 
+import Header from './Header';
+import Show from './Show';
+import Empty from './Empty';
+
 //------------------------------------------------------------------------------
 // Props:
-//   -
+//   - time: String
+//   - interview: Object
+//   - onEdit(): Function
+//   - onDelete(): Function
 
 const Appointment = function (props) {
-  return <article className="appointment"></article>;
+  const content = props.interview ? (
+    <Show
+      student={props.interview.student}
+      interviewer={props.interview.interviewer}
+      onEdit={props.onEdit}
+      onDelete={props.onDelete}
+    />
+  ) : (
+    <Empty onAdd={props.onAdd} />
+  );
+
+  return (
+    <article className="appointment">
+      <Header time={props.time} />
+      {content}
+    </article>
+  );
 };
 
 export default Appointment;
--- stories/index.js
+++ stories/index.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Fragment } from 'react';
 
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
@@ -166,6 +166,22 @@ storiesOf('Appointment', module)
   })
   .add('Appointment', () => <Appointment />)
   .add('Appointment with time', () => <Appointment time="12pm" />)
+  .add('Appointment Empty', () => (
+    <Fragment>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
+  .add('Appointment Booked', () => (
+    <Fragment>
+      <Appointment
+        id={1}
+        time="12pm"
+        interview={{ student: 'Lydia Miller-Jones', interviewer }}
+      />
+      <Appointment id="last" time="1pm" />
+    </Fragment>
+  ))
   .add('Header', () => <Header time="12pm" />)
   .add('Confirm', () => (
     <Confirm
