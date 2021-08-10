a0b43d7e94975e37bef436b0e325d48646bdaf61
Yves Candau <ycandau@sfu.ca>
2021-08-09 23:30:27 -0700

    Complete integration test: data loading, appointment booking, and spots update

--- src/components/DayListItem.js
+++ src/components/DayListItem.js
@@ -4,25 +4,25 @@ import classNames from 'classnames';
 import 'components/DayListItem.scss';
 
 //------------------------------------------------------------------------------
-// Props:
-//   - name: String
-//   - spots: Number
-//   - selected: Boolean
-//   - setDay(): Function
 
-const DayListItem = (props) => {
-  const formatSpots = (spots) =>
-    spots === 0 ? 'no spots' : spots === 1 ? '1 spot' : `${spots} spots`;
+const DayListItem = ({ name, spots, selected, setDay }) => {
+  const formatSpots = (spt) =>
+    spt === 0 ? 'no spots' : spt === 1 ? '1 spot' : `${spt} spots`;
 
   const dayClass = classNames('day-list__item', {
-    'day-list__item--selected': props.selected,
-    'day-list__item--full': props.spots === 0,
+    'day-list__item--selected': selected,
+    'day-list__item--full': spots === 0,
   });
 
   return (
-    <li className={dayClass} onClick={props.setDay} selected={props.selected}>
-      <h2 className="text--regular">{props.name}</h2>
-      <h3 className="text--light"> {formatSpots(props.spots)} remaining</h3>
+    <li
+      className={dayClass}
+      onClick={setDay}
+      selected={selected}
+      data-testid="day"
+    >
+      <h2 className="text--regular">{name}</h2>
+      <h3 className="text--light"> {formatSpots(spots)} remaining</h3>
     </li>
   );
 };
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -10,6 +10,7 @@ import {
   waitForElement,
   fireEvent,
   prettyDOM,
+  queryByText,
 } from '@testing-library/react';
 
 import Application from 'components/Application';
@@ -32,14 +33,22 @@ describe('Application', () => {
     const { container } = render(<Application />);
 
     await waitForElement(() => getByText(container, 'Archie Cohen'));
-    const appointment = getAllByTestId(container, 'appointment')[0];
 
+    const appointment = getAllByTestId(container, 'appointment')[0];
     fireEvent.click(getByAltText(appointment, 'Add'));
+
     const input = getByTestId(appointment, 'student-name-input');
     fireEvent.change(input, { target: { value: 'Lydia' } });
     fireEvent.click(getByAltText(appointment, 'Sylvia Palmer'));
     fireEvent.click(getByText(appointment, 'Save'));
 
-    console.log(prettyDOM(appointment));
+    expect(getByText(appointment, 'Saving')).toBeInTheDocument();
+
+    await waitForElement(() => getByText(appointment, 'Lydia'));
+
+    const days = getAllByTestId(container, 'day');
+    const day = days.find((d) => queryByText(d, 'Monday'));
+
+    expect(getByText(day, 'no spots remaining')).toBeInTheDocument();
   });
 });
